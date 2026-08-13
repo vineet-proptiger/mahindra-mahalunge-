6f2dc014d3d67c82800a7fb878c1d04995ca9646
vineet-proptiger <vineet.mishra@proptiger.com>
2026-08-13 12:29:38 +0530

    some fixes

--- components/Location.jsx
+++ components/Location.jsx
@@ -91,9 +91,7 @@ const Location = () => {
             }}>
               <iframe 
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d242064.6412893556!2d73.43892138671875!3d18.56231250000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bd23899c4255%3A0xd1c64c7ae74e9a5!2sMahindra%20Lifespaces%20in%20Mahalunge!5e0!3m2!1sen!2sin!4v1786593710699!5m2!1sen!2sin" 
-                width="100%" 
-                height="100%" 
-                style={{ border: 0 }} 
+                style={{ border: 0, position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} 
                 allowFullScreen="" 
                 loading="lazy" 
                 referrerPolicy="no-referrer-when-downgrade"
